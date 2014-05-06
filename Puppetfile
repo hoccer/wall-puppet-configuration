397fec4df70b48ce14afc244130d1d78cf2fd0ef
Mirko Geissler <mirko.geissler@artcom.de>
2014-05-06 12:14:18 +0200

    Add provisioning files

---
+++ Puppetfile
@@ -0,0 +1,7 @@
+forge "http://forge.puppetlabs.com"
+
+mod 'puppetlabs/stdlib'
+mod 'backuppc-client', :git => 'https://github.com/hoccer/puppet-backuppc-client.git'
+mod 'deployment-user', :git => 'https://github.com/hoccer/puppet-deployment-user.git'
+mod 'nrpe', :git => 'https://github.com/hoccer/puppet-nrpe.git'
+mod 'java', :git => 'https://github.com/puppetlabs/puppetlabs-java.git'
---
+++ init.pp
@@ -0,0 +1,36 @@
+define line($file, $line, $ensure = 'present') {
+    case $ensure {
+        default : { err ( "unknown ensure value ${ensure}" ) }
+        present: {
+            exec { "/bin/echo '${line}' >> '${file}'":
+                unless => "/bin/grep -qFx '${line}' '${file}'"
+            }
+        }
+        absent: {
+            exec { "/bin/grep -vFx '${line}' '${file}' | /usr/bin/tee '${file}' > /dev/null 2>&1":
+              onlyif => "/bin/grep -qFx '${line}' '${file}'"
+            }
+
+            # Use this resource instead if your platform's grep doesn't support -vFx;
+            # note that this command has been known to have problems with lines containing quotes.
+            # exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+            #     onlyif => "/bin/grep -qFx '${line}' '${file}'"
+            # }
+        }
+    }
+}
+
+
+include backuppc-client
+include deployment-user
+include nrpe
+include java
+file_line { 'urandom fix':
+  path  => '/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/security/java.security',
+  line  => 'securerandom.source=file:/dev/./urandom',
+  match => '^securerandom.source=.*',
+}
+
+package { "pwgen":
+    ensure => "installed"
+}
